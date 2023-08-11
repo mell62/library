01dd672b6d050dc88eabf98f75f41461af2f2f1a
Mridhul <mridhul67@gmail.com>
2023-08-11 15:16:58 +0530

    Add a google font for main content

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        exo: ['"Exo"'],
+        milonga: ['"Milonga"'],
+        redhat: ["Red Hat Text"],
       },
     },
   },
