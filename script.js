a8ee1ed7a5b10eea80eb5ff4f239a9aada5d861c
Mridhul <mridhul67@gmail.com>
2023-08-15 14:38:40 +0530

    Refactor the code to activate modal using dialog

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 const addBtn = document.querySelector(".add-btn");
-const modalContainer = document.querySelector(".modal-container");
+const modal = document.querySelector(".modal");
 const form = document.querySelector(".book-form");
-const cancelBtn = document.querySelector(".cancel-btn");
 
-addBtn.addEventListener("click", modalAppear);
-
-form.addEventListener("submit", (event) => event.preventDefault());
-form.addEventListener("submit", modalDisappear);
-
-cancelBtn.addEventListener("click", modalDisappear);
-
-function modalAppear() {
-  modalContainer.classList.remove("hidden");
-  modalContainer.classList.add("flex", "justify-center", "items-center");
-}
-
-function modalDisappear() {
-  modalContainer.classList.add("hidden");
-  modalContainer.classList.remove("flex", "justify-center", "items-center");
-}
+addBtn.addEventListener("click", () => modal.showModal());
