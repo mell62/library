82015ac2761da8b2da4c635083f235c63c0cda3a
Mridhul <mridhul67@gmail.com>
2023-08-15 15:08:23 +0530

    Close modal when cancel is clicked

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const addBtn = document.querySelector(".add-btn");
 const modal = document.querySelector(".modal");
 const form = document.querySelector(".book-form");
+const cancelBtn = document.querySelector(".cancel-btn");
 
 addBtn.addEventListener("click", () => modal.showModal());
 
 form.addEventListener("submit", (event) => event.preventDefault());
 form.addEventListener("submit", () => modal.close());
+
+cancelBtn.addEventListener("click", () => modal.close());
