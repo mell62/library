83243daeb5ac95781641f4a867dcf286b886339b
Mridhul <mridhul67@gmail.com>
2023-08-15 13:13:16 +0530

    Deactivate modal upon clicking submit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const addBtn = document.querySelector(".add-btn");
 const modalContainer = document.querySelector(".modal-container");
+const form = document.querySelector(".book-form");
 
 addBtn.addEventListener("click", modalAppear);
 
-// function modalAppear() {
-//   modalContainer.classList.remove("hidden");
-//   modalContainer.classList.add("flex", "justify-center", "items-center");
-// }
+form.addEventListener("submit", (event) => event.preventDefault());
+form.addEventListener("submit", modalDisappear);
 
 function modalAppear() {
   modalContainer.classList.remove("hidden");
   modalContainer.classList.add("flex", "justify-center", "items-center");
+}
+
+function modalDisappear() {
+  modalContainer.classList.add("hidden");
+  modalContainer.classList.remove("flex", "justify-center", "items-center");
 }
