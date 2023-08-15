67af6e1a5c64d78714ad70f7540026e02a198672
Mridhul <mridhul67@gmail.com>
2023-08-15 14:42:48 +0530

    Close the modal upon clicking submit

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const modal = document.querySelector(".modal");
 const form = document.querySelector(".book-form");
 
 addBtn.addEventListener("click", () => modal.showModal());
+
+form.addEventListener("submit", (event) => event.preventDefault());
+form.addEventListener("submit", () => modal.close());
