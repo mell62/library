c4389f7a42b6fed5a955ead2ada140be1c7acaec
Mridhul <mridhul67@gmail.com>
2023-08-16 19:06:55 +0530

    Implement functions to store book details in an array upon form submission

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ const cancelBtn = document.querySelector(".cancel-btn");
 addBtn.addEventListener("click", () => modal.showModal());
 
 form.addEventListener("submit", (event) => event.preventDefault());
+form.addEventListener("submit", addToLibrary);
 form.addEventListener("submit", () => modal.close());
 
 cancelBtn.addEventListener("click", () => modal.close());
+
+const library = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+function addToLibrary() {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const status = document.querySelector("#status").checked;
+
+  const newBook = new Book(title, author, pages, status);
+  library.push(newBook);
+}
