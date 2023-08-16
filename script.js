e42b2d65c39cf118b77cab814a2a4bfaaecf852e
Mridhul <mridhul67@gmail.com>
2023-08-16 19:44:37 +0530

    Implement functions to add a new row and cells to table upon form submission

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const addBtn = document.querySelector(".add-btn");
 const modal = document.querySelector(".modal");
 const form = document.querySelector(".book-form");
 const cancelBtn = document.querySelector(".cancel-btn");
+const tbody = document.querySelector("tbody");
+const library = [];
 
 addBtn.addEventListener("click", () => modal.showModal());
 
 form.addEventListener("submit", (event) => event.preventDefault());
 form.addEventListener("submit", addToLibrary);
+form.addEventListener("submit", addRow);
+form.addEventListener("submit", addCells);
 form.addEventListener("submit", () => modal.close());
 
 cancelBtn.addEventListener("click", () => modal.close());
-
-const library = [];
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -25,7 +27,24 @@ function addToLibrary() {
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
   const status = document.querySelector("#status").checked;
-
   const newBook = new Book(title, author, pages, status);
+
   library.push(newBook);
+}
+
+function addRow() {
+  const tr = document.createElement("tr");
+  tbody.appendChild(tr);
+}
+
+function addCells() {
+  const trList = document.querySelectorAll("tr");
+  const newRow = trList[trList.length - 1];
+  const th = document.createElement("th");
+
+  newRow.appendChild(th);
+  for (let i = 0; i < 4; i++) {
+    const td = document.createElement("td");
+    newRow.appendChild(td);
+  }
 }
