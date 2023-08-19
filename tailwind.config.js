79bc641d1a037083feace9869ec4ffab576e36be
Mridhul <mridhul67@gmail.com>
2023-08-20 00:24:31 +0530

    Replace header font with a new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        milonga: ['"Milonga"'],
+        bricolage: ['"Bricolage Grotesque"'],
         redhat: ["Red Hat Text"],
       },
     },
